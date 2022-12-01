d88bf24abd71e33fe4e35adead241077115a4b70
Random2142Max <104144843+Random2142Max@users.noreply.github.com>
2022-12-01 16:17:07 +0500

    Добавил модели данных на основе ведомости + сохранил диаграмму в виде картинки

---
+++ src/main/java/org/example/Models/Task.java
@@ -0,0 +1,16 @@
+package org.example.Models;
+
+public class Task {
+    public int Pk_Id;
+    public String Name;
+    public int Value;
+    public int MaxValue;
+    public int Fk_id_TypeTask;
+
+    public Task(String name, int maxValue, String Fk_id_typeTask) {
+        this.Name = name;
+        this.Value = 0;
+        this.MaxValue = maxValue;
+        this.Fk_id_TypeTask = Integer.parseUnsignedInt(Fk_id_typeTask);
+    }
+}
