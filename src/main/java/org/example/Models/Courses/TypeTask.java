13c3a1d8861aea663b39bfe321ab3d6f64abddcb
Random2142Max <104144843+Random2142Max@users.noreply.github.com>
2022-12-07 14:51:01 +0500

    Изменения: 1) Добавлен Класс Report для парсинга 2) Добавлен парсинг моделей из basicprogramming_2.csv в Report; 3) В парсинге реализовано создание объектов Student,TypeTask,Task.

--- src/main/java/org/example/Models/Courses/TypeTask.java
+++ src/main/java/org/example/Models/Courses/TypeTask.java
@@ -1,5 +1,12 @@
 package org.example.Models.Courses;
 
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.List;
+
 enum TypeTaskEnum {
     ACTIVE,
     EXERCISE,
@@ -12,6 +19,8 @@ public class TypeTask {
     private TypeTaskEnum typeTask;
 
     public TypeTask(String typeTask) {
+        // Проверка на кодировку, так как ранее она ломалась
+        //System.out.println(Charset.defaultCharset());
         switch (typeTask.toUpperCase()) {
             case "АКТ":
                 this.typeTask = TypeTaskEnum.ACTIVE;
@@ -29,7 +38,14 @@ public class TypeTask {
                 this.typeTask = TypeTaskEnum.ADDITIONALLY;
                 break;
             default:
-                throw new IllegalArgumentException(String.format("В ведомости указан %s. Данный тип заданий отсутствует!", typeTask));
+                throw new IllegalArgumentException((String.format("В ведомости указан %s. Данный тип заданий отсутствует!", typeTask)));
+//                try {
+//                    throw new IllegalArgumentException((String.format("В ведомости указан %s. Данный тип заданий отсутствует!", typeTask)));
+//                }
+//                catch (RuntimeException e) {
+//                    System.out.println(typeTask);
+//                    e.printStackTrace();
+//                }
         }
     }
 
